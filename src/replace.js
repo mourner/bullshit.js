b0dcd56806afe907c0c0d5a839c0892886a11e4d
Vladimir Agafonkin <agafonkin@gmail.com>
2013-01-10 18:48:05 +0200

    minor code cleanup

--- src/replace.js
+++ src/replace.js
@@ -1,17 +1,21 @@
+
 function revealBullshit(term) {
+
     var c = term.charAt(0),
         bullshit = (c === c.toUpperCase() ? 'B' : 'b') + 'ullshit';
 
     if (term.substr(term.length - 3) === 'ing') {
         bullshit += 'ing';
     }
+
     var abbr = document.createElement("abbr");
-    abbr.setAttribute("style", "color:red");
+    abbr.style.color = 'red';
     abbr.title = term;
     abbr.innerHTML = bullshit;
+
     return abbr;
 }
 
-findAndReplaceDOMText(
-        new RegExp('(' + bullshitTerms.join('|') + ')(?!\\w|[^<]*>)', 'gi'),
-        document.body, revealBullshit);
+var bullshitRe = new RegExp('(' + bullshitTerms.join('|') + ')(?!\\w|[^<]*>)', 'gi');
+
+findAndReplaceDOMText(bullshitRe, document.body, revealBullshit);
