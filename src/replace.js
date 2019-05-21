4dae3c5943133ad43a0f05d81770d75ee8f32ecb
Vladimir Agafonkin <agafonkin@gmail.com>
2019-05-21 13:35:24 +0300

    more fixes and modernization

--- src/replace.js
+++ src/replace.js
@@ -1,30 +1,33 @@
 import findAndReplaceDOMText from 'findandreplacedomtext';
 import bullshitTerms from './terms';
 
-function revealBullshit(term) {
+function revealBullshit({text}) {
+    const c = text.charAt(0);
+    const last = text.length - 1;
+    let bullshit = `${c === c.toUpperCase() ? 'B' : 'b'}ullshit`;
 
-    const c = term.charAt(0);
-    const last = term.length - 1;
-    let bullshit = `(${c === c.toUpperCase() ? 'B' : 'b'}ullshit`;
-
-    if (term.substr(last - 2) === 'ing') {
+    if (text.substr(last - 2) === 'ing') {
         bullshit += 'ting';
-    } else if (term.charAt(last - 1) !== 's' && term.charAt(last) === 's') {
+    } else if (text.charAt(last - 1) !== 's' && text.charAt(last) === 's') {
         bullshit += 's';
-    } else if (term.charAt(last - 2) !== 'e' && term.substr(last - 1) === 'ed') {
+    } else if (text.charAt(last - 2) !== 'e' && text.substr(last - 1) === 'ed') {
         bullshit += 'ted';
-    } else if (term.charAt(last - 2) !== ('o' || 'e') && term.substr(last - 1) === ('or' || 'er')) {
+    } else if (text.charAt(last - 2) !== ('o' || 'e') && text.substr(last - 1) === ('or' || 'er')) {
         bullshit += 'ter';
     }
 
     const abbr = document.createElement('abbr');
     abbr.style.color = 'red';
-    abbr.title = term;
+    abbr.title = text;
     abbr.innerHTML = bullshit;
 
     return abbr;
 }
 
-const bullshitRe = new RegExp(`(${bullshitTerms.join('|')})(?!\\w|[^<]*>)`, 'gi');
+const bullshitRe = new RegExp(`\\b(${bullshitTerms.join('|')})\\b`, 'gi');
 
-findAndReplaceDOMText(bullshitRe, document.body, revealBullshit);
+findAndReplaceDOMText(document.body, {
+    find: bullshitRe,
+    replace: revealBullshit,
+    preset: 'prose'
+});
