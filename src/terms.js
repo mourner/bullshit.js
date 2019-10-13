2bb1b82ed92d84aae2d4c405f09199e39608c37a
shyorioni7 <52894379+shyorioni7@users.noreply.github.com>
2019-10-13 02:05:10 -0700

    Update src/terms.js
    Co-Authored-By: Vladimir Agafonkin <agafonkin@gmail.com>

--- src/terms.js
+++ src/terms.js
@@ -303,7 +303,7 @@ export default [
     'significant(ly)?',
     'single-source responsibility',
     'skill ?sets?',
-    'smart(er)',
+    'smart(er)?',
     'smoke (&|and) mirrors',
     'social(.media|.gaming|.networks?)',
     'solutions?',
