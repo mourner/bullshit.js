c6fe29570493a25580e10eb8a0edaa19bdcfcd05
Remarknl <richardginus@gmail.com>
2019-10-14 13:24:12 +0200

    Add "company-employee fit"
    This bullshit is what our marketing said when an intern used bullshit.js on our corporate website.

--- src/terms.js
+++ src/terms.js
@@ -56,6 +56,7 @@ export default [
     'cloud',
     'collaborat(e|ion|ive)s?',
     'communicat(e|ion)s?',
+    'company-employee.fit',
     'compelling',
     'competitive( advantage)?',
     'complex',
